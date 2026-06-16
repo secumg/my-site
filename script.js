8cf3f6185e26d00774c6d42c3da700d427f95ecf
secumg <investornetty@gmail.com>
2026-06-16 17:26:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", function () {
+
     alert("JS IS RUNNING");
 
     const btn = document.getElementById("connectBtn");
     const output = document.getElementById("output");
 
+    if (!btn || !output) {
+        alert("Missing HTML elements");
+        return;
+    }
+
     btn.addEventListener("click", function () {
         output.innerText = "Button clicked successfully!";
     });
-};
+
+});
